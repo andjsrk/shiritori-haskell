d700fc8824a11f198cc764f2db903f9cd361533c
andjsrk <andjsrk0213@gmail.com>
2023-07-17 09:21:42 +0900

    change word (`check` to `require`)

--- app/Main.hs
+++ app/Main.hs
@@ -5,30 +5,28 @@ import Data.Foldable
 f .> g = g . f
 
 type ErrorMsg = String
-isFalseThen :: Bool -> ErrorMsg -> Maybe ErrorMsg
-expected `isFalseThen` errorMsg =
-  if expected
+type Requirement = String -> [String] -> Maybe ErrorMsg
+require :: Bool -> ErrorMsg -> Maybe ErrorMsg
+require expected errorMsg =
+   if expected
     then Nothing
     else Just errorMsg
 
-checkTooShort word _ =
-  (2 <= length word) `isFalseThen`
-    "A word must be 2 or more characters."
+requireNotTooShort word _ =
+  require (2 <= length word) "A word must be 2 or more characters."
 
-checkWordDoesNotStartWithLastCharOfLastUsedWord _ [] = Nothing
-checkWordDoesNotStartWithLastCharOfLastUsedWord word (lastUsedWord:_) =
-  (head word == last lastUsedWord) `isFalseThen`
-    (word ++ " does not start with " ++ [last lastUsedWord] ++ ".")
+requireWordStartsWithLastCharOfLastUsedWord _ [] = Nothing
+requireWordStartsWithLastCharOfLastUsedWord word (lastUsedWord:_) =
+  require (head word == last lastUsedWord) (word ++ " does not start with " ++ [last lastUsedWord] ++ ".")
 
-checkAlreadyUsed word usedWords =
-  (word `notElem` usedWords) `isFalseThen`
-    (word ++ " is already used.")
+requireNotAlreadyUsed word usedWords =
+  require (word `notElem` usedWords) (word ++ " is already used.")
 
-checks :: [String -> [String] -> Maybe ErrorMsg]
-checks = [
-    checkTooShort,
-    checkWordDoesNotStartWithLastCharOfLastUsedWord,
-    checkAlreadyUsed
+requirements :: [String -> [String] -> Maybe ErrorMsg]
+requirements = [
+    requireNotTooShort,
+    requireWordStartsWithLastCharOfLastUsedWord,
+    requireNotAlreadyUsed
   ]
 
 processTurn usedWords = do
@@ -42,6 +40,6 @@ processTurn usedWords = do
       (\msg -> do
         putStrLn msg
         processTurn usedWords)
-      (asum $ map (($ word) .> ($ usedWords)) checks)
+      (asum $ map (($ word) .> ($ usedWords)) requirements)
 
 main = processTurn []
